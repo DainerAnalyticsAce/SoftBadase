2fd4247f54658d62cbc1c8448816a1f0b3205176
Dainer Duran Analyst <168243742+DainerAnalyticsAce@users.noreply.github.com>
2024-04-27 12:07:40 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+use [Softbadase(Nuevo)]
+--select*from Tbl_RegistrarElemento
+--SELECT Tbl_RegistrarPersona.Cedula, Nombres,Primer_Apellido, Elemento, Serie_Del_Elemento, Descripcion, EstadoRegistro FROM Tbl_RegistrarElemento inner join Tbl_RegistrarPersona on Tbl_RegistrarElemento.Cedula=Tbl_RegistrarPersona.Cedula
+--select * from Tbl_RegistrarElemento
+--select * from  Tbl_MovimientosDeElementosRegistrosSoftbadase
+--select*from Tbl_RegistrarElementoInstructoresEmpleadores
+--select * from Tbl_MovimientosDeElementosRegistrosSoftbadaseInstructoresEmple
+ --select * from Tbl_MovimientosDeElementosRegistrosSoftbadaseInstructoresEmple 
+--delete from Tbl_MovimientosDeElementosRegistrosSoftbadaseInstructoresEmple
+--select * from Tbl_MovimientosDeElementosRegistrosSoftbadase
+select * from Tbl_MovimientosDeElementosRegistrosSoftbadaseInstructoresEmple
